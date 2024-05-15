fc790e32d71e185e602abcb94cee476c94a2843f
PatrickZablocki <Patrick Zablocki>
2024-05-15 11:32:22 +0200

    Hover bearbeitet und nun daran arbeiten das der Hamburger funktioniert

--- webkids/src/components/Navbar.jsx
+++ webkids/src/components/Navbar.jsx
@@ -25,19 +25,19 @@ const Navbar = () => {
             <div className={`${styles.nav_icons} ${showMenu ? styles.show : ''}`}>
                 <span className={styles.icon}>
                     <FontAwesomeIcon icon={faHome} />
-                    <h3>Start</h3>
+                    <a href="">Start</a>
                 </span>
                 <span className={styles.icon}>
                     <FontAwesomeIcon icon={faUserFriends} />
-                    <h3>Ihr Netzwerk</h3>
+                    <a href="">Ihr Netzwerk</a>
                 </span>
                 <span className={styles.icon}>
                     <FontAwesomeIcon icon={faEnvelope} />
-                    <h3>Nachrichten</h3>
+                    <a href="">Nachrichten</a>
                 </span>
                 <span className={styles.icon}>
                     <FontAwesomeIcon icon={faBell} />
-                    <h3>Mitteilungen</h3>
+                    <a href="">Mitteilungen</a>
                 </span>
                 {isLoggedIn ? (
                     <span className={styles.profile_picture}>
