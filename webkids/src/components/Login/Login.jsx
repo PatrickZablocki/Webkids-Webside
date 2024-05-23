8c5cedc5f827dafd96d063859f5fae2d5a0a659f
PatrickZablocki <Patrick Zablocki>
2024-05-23 10:18:41 +0200

    Das Frontend ist soweit fertig von der Login Page sowie von der Register Page, nun schaue ich das ich die anmeldungen im Backen umsetzten kann

--- webkids/src/components/Login/Login.jsx
+++ webkids/src/components/Login/Login.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import styles from "./Login.module.css";
 
 function Login() {
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
-const handleLogin = (event) => {
-    event.preventDefault();
-    
-    navigate('/login');
-};
+    const handleLogin = (event) => {
+        event.preventDefault();
+        navigate('/login');
+    };
 
-const handleLoginWithGoogle = () =>{
-    console.log('Anmeldung mit Google');
-}
+    const handleLoginWithGoogle = () => {
+        console.log('Anmeldung mit Google');
+    };
 
-const handleLoginWithApple = () => {
-    console.log('Anmeldung mit Apple');
-}
+    const handleLoginWithApple = () => {
+        console.log('Anmeldung mit Apple');
+    };
 
-return (
-    <div className={styles.Login_container}>
-        <div className={styles.Login_content}>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-            <input type="text" placeholder="Username" required />
-            <input type="password" placeholder="Password" required />
-            <button type="submit">Log In</button>
-            </form>
-            <hr/>
-            <div className={styles.Login_Social}>
-            <button className={styles.google_button} onClick={handleLoginWithGoogle}>
-                    <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
-                    Mit Google anmelden
-            </button>
-            <button className={styles.apple_button} onClick={handleLoginWithApple}>
-                    <FontAwesomeIcon icon={faApple} className={styles.icon} />
-                    Mit Apple anmelden
-            </button>
-            <hr/>
+    return (
+        <div className={styles.Login_container}>
+            <div className={styles.Login_box}>
+                <div className={styles.Login_content}>
+                    <h1>Login</h1>
+                    <form onSubmit={handleLogin}>
+                        <input type="text" placeholder="Username" required />
+                        <input type="password" placeholder="Password" required />
+                        <button type="submit">Anmelden</button>
+                    </form>
+                    <hr />
+                    <div className={styles.Login_Social}>
+                        <button className={styles.google_button} onClick={handleLoginWithGoogle}>
+                            <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
+                            Mit Google anmelden
+                        </button>
+                        <button className={styles.apple_button} onClick={handleLoginWithApple}>
+                            <FontAwesomeIcon icon={faApple} className={styles.icon} />
+                            Mit Apple anmelden
+                        </button>
+                    </div>
+                    <hr/>
+                    <div className={styles.Login_toRegister}>
+                    <p>Du besitzt keinen Account? <Link to="/register">Registriere dich</Link></p>
+                    </div>
+                </div>
+                <div className={styles.Login_image}></div>
             </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default Login;
--- webkids/src/components/Register/Register.jsx
+++ webkids/src/components/Register/Register.jsx
@@ -1,21 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
+import { useNavigate, Link } from 'react-router-dom';
+import styles from './Register.module.css';
 
 function Register() {
-  const handleRegister = (event) => {
-    event.preventDefault();
-    
-  };
+    const navigate = useNavigate();
+    const [birthdate, setBirthdate] = useState('');
 
-  return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <input type="text" placeholder="Username" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
+    const handleRegister = (event) => {
+        event.preventDefault();
+        console.log('Registration successful');
+        navigate('/login');
+    };
+
+    const handleLoginWithGoogle = () => {
+        console.log('Anmeldung mit Google');
+    };
+
+    const handleLoginWithApple = () => {
+        console.log('Anmeldung mit Apple');
+    };
+
+    return (
+        <div className={styles.Register_container}>
+            <div className={styles.Register_box}>
+                <div className={styles.Register_image}></div>
+                <div className={styles.Register_content}>
+                    <h1>Registrieren</h1>
+                    <form onSubmit={handleRegister}>
+                        <input type="text" placeholder="Username" required />
+                        <input type="text" placeholder="Vorname" required />
+                        <input type="text" placeholder="Nachname" required />
+                        <label>Geburtsdatum:</label>
+                        <input 
+                            type="date" 
+                            value={birthdate} 
+                            onChange={(e) => setBirthdate(e.target.value)} 
+                            required 
+                        />
+                        <input type="password" placeholder="Password" required />
+                        <button type="submit">Registrieren</button>
+                    </form>
+                    <hr />
+                    <div className={styles.Register_Social}>
+                        <button className={styles.google_button} onClick={handleLoginWithGoogle}>
+                            <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
+                            Mit Google registrieren
+                        </button>
+                        <button className={styles.apple_button} onClick={handleLoginWithApple}>
+                            <FontAwesomeIcon icon={faApple} className={styles.icon} />
+                            Mit Apple registrieren
+                        </button>
+                    </div>
+                    <hr/>
+                    <div className={styles.Register_toRegister}>
+                        <p>Du besitzt bereits einen Account? <Link to="/login">Anmelden</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Register;
